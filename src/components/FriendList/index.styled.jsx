94fb3c0c4508bb724d7cf61bd0603e7e92f7a4a2
Pavel <parbeck17@gmail.com>
2022-07-08 08:59:40 +0300

    start

--- src/components/FriendList/index.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const FriendListBox = styled.ul`
-   width: 320px;
-   max-width: 100%;
-   margin: 0px auto 40px auto;
-   list-style-type: none;
-   padding: 0px;
-`;
